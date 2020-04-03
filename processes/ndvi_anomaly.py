8c3160ef673e567dace4b9c0191294f24193792c
Daniel Tipping <daniel@oldreliable.tech>
2020-04-03 11:11:00 +0100

    Update ndvi_anomaly to use dask
    It looks like the underlying process has changed significantly since the
    original script was written. This commit updates it as well as
    converting it to use dask.
    
    Two new params added: crs and mosaic_type.
    
    This commit also adds a `mask_good_quality` function. This is used in
    all of the notebooks so it's pulled into a separate file.

--- processes/ndvi_anomaly.py
+++ processes/ndvi_anomaly.py
@@ -1,16 +1,21 @@
 import numpy as np
 import xarray as xr
 import re
+import dask
 from os import path
+from pyproj import Proj, transform
 
 from cubequery.tasks import CubeQueryTask, Parameter, DType
-from datacube_utilities.dc_utilities import write_geotiff_from_xr
-from datacube_utilities.dc_water_classifier import wofs_classify
 from datacube_utilities import import_export
 from datacube_utilities.createindices import NDVI
-from datacube_utilities.clean_mask import landsat_qa_clean_mask
 from datacube_utilities.createAOI import create_lat_lon
-from datacube_utilities.dc_mosaic import create_median_mosaic
+from datacube_utilities.dc_mosaic import (
+    create_median_mosaic,
+    create_mean_mosaic,
+    create_max_ndvi_mosaic,
+)
+
+from utils.masking import mask_good_quality
 
 
 class NDVIAnomaly(CubeQueryTask):
@@ -82,6 +87,14 @@ class NDVIAnomaly(CubeQueryTask):
             "Pixel resution in meters",
             [0, 500],
         ),
+        Parameter("crs", "CRS", DType.STRING, "CRS to use for the analysis"),
+        Parameter(
+            "mosaic_type",
+            "Mosaic Type",
+            DType.STRING,
+            "Mosaic type to use for the analysis",
+            ["median", "mean", "max_ndvi"],
+        ),
     ]
 
     CubeQueryTask.cal_significant_kwargs(parameters)
@@ -99,27 +112,51 @@ class NDVIAnomaly(CubeQueryTask):
         platform_base,
         platform_analysis,
         res,
+        crs,
+        mosaic_type,
         **kwargs,
     ):
+
+        ## Create datacube query
+
         lat_extents, lon_extents = create_lat_lon(aoi)
+        inProj = Proj("+init=EPSG:4326")
+        outProj = Proj("+init=EPSG:3460")
+
+        min_lat, max_lat = lat_extents
+        min_lon, max_lon = lon_extents
+
+        x_A, y_A = transform(inProj, outProj, min_lon, min_lat)
+        x_B, y_B = transform(inProj, outProj, max_lon, max_lat)
+
+        lat_range = (y_A, y_B)
+        lon_range = (x_A, x_B)
+
+        resolution = (-res, res)
 
         all_measurements = ["green", "red", "blue", "nir", "swir1", "swir2"]
-        baseline_product, baseline_measurement = create_product_measurement(
+        baseline_product, baseline_measurement, baseline_water_product = create_product_measurement(
             platform_base, all_measurements
         )
-        analysis_product, analysis_measurement = create_product_measurement(
+        analysis_product, analysis_measurement, analysis_water_product = create_product_measurement(
             platform_analysis, all_measurements
         )
 
         baseline_time_period = (baseline_start_date, baseline_end_date)
         analysis_time_period = (analysis_start_date, analysis_end_date)
 
+        dask_chunks = dict(time=40, x=2000, y=2000)
+
         query = {
-            "y": lat_extents,
-            "x": lon_extents,
+            "y": lat_range,
+            "x": lon_range,
             "output_crs": projection,
-            "resolution": [float(res), float(res)],
+            "resolution": resolution,
+            "dask_chunks": dask_chunks,
+            "crs": crs,
         }
+
+        ## Create dask graph
 
         baseline_ds = dc.load(
             time=baseline_time_period,
@@ -149,65 +186,73 @@ class NDVIAnomaly(CubeQueryTask):
                 + "Please check load parameters for Analysis Dataset!"
             )
 
-        baseline_clean_mask = landsat_qa_clean_mask(baseline_ds, platform_base)
-        analysis_clean_mask = landsat_qa_clean_mask(analysis_ds, platform_analysis)
+        if platform_base in ["LANDSAT_8", "LANDSAT_7", "LANDSAT_5", "LANDSAT_4"]:
+            water_scenes_baseline = dc.load(
+                product=baseline_water_product,
+                measurements=["water_classification"],
+                **query,
+            )
+            water_scenes_analysis = dc.load(
+                product=analysis_water_product,
+                measurements=["water_classification"],
+                **query,
+            )
+        else:
+            raise Exception("S2 does not yet have daskable water classification")
 
-        baseline_ds = baseline_ds.where(baseline_clean_mask)
-        analysis_ds = analysis_ds.where(analysis_clean_mask)
+        b_good_quality = mask_good_quality(platform_base, baseline_ds)
+        a_good_quality = mask_good_quality(platform_analysis, analysis_ds)
 
-        baseline_composite = create_median_mosaic(
-            baseline_ds, clean_mask=baseline_clean_mask
-        )
-        analysis_composite = create_median_mosaic(
-            analysis_ds, clean_mask=analysis_clean_mask
+        baseline_ds = baseline_ds.where(b_good_quality)
+        analysis_ds = analysis_ds.where(a_good_quality)
+
+        mosaic_function = {
+            "median": create_median_mosaic,
+            "mean": create_mean_mosaic,
+            "max_ndvi": create_max_ndvi_mosaic,
+        }
+
+        new_compositor = mosaic_function[mosaic_type]
+
+        if mosaic_type == "median" or mosaic_type == "mean":
+            # the mean and medan functions work automatically with dask without using `dask.delayed`
+            # because they exclusively use xarray functions which already support dask.
+            # this gives us a ~20% time saving on small datasets
+            baseline_composite = new_compositor(baseline_ds, clean_mask=b_good_quality)
+            analysis_composite = new_compositor(analysis_ds, clean_mask=a_good_quality)
+        else:
+            baseline_composite = dask.delayed(new_compositor)(
+                baseline_ds, clean_mask=b_good_quality
+            )
+            analysis_composite = dask.delayed(new_compositor)(
+                analysis_ds, clean_mask=a_good_quality
+            )
+
+        water_classes_base = water_scenes_baseline.where(water_scenes_baseline >= 0)
+        water_classes_analysis = water_scenes_analysis.where(water_scenes_analysis >= 0)
+
+        water_composite_base = water_classes_base.water_classification.mean(dim="time")
+        water_composite_analysis = water_classes_analysis.water_classification.mean(
+            dim="time"
         )
 
-        water_class_base = wofs_classify(
-            baseline_composite, mosaic=True, x_coord="x", y_coord="y"
-        ).wofs
-        baseline_composite = baseline_composite.copy(deep=True).where(
-            (baseline_composite != np.nan) & (water_class_base == 0)
+        baseline_composite = baseline_composite.where(
+            (baseline_composite != np.nan) & (water_composite_base == 0)
         )
-        water_class_analy = wofs_classify(
-            analysis_composite, mosaic=True, x_coord="x", y_coord="y"
-        ).wofs
-        analysis_composite = analysis_composite.copy(deep=True).where(
-            (analysis_composite != np.nan) & (water_class_analy == 0)
+        analysis_composite = analysis_composite.where(
+            (analysis_composite != np.nan) & (water_composite_analysis == 0)
         )
 
         ndvi_baseline_composite = NDVI(baseline_composite)
         ndvi_analysis_composite = NDVI(analysis_composite)
 
-        result = []
-        file_name = path.join(path_prefix, "ndvi_baseline.tiff")
-        ndvi_baseline_export = xr.DataArray.to_dataset(
-            ndvi_baseline_composite, dim=None, name="ndvi_baseline"
-        )
-        import_export.export_xarray_to_geotiff(
-            ndvi_baseline_export,
-            file_name,
-            bands=["ndvi_baseline"],
-            crs=projection,
-            x_coord="x",
-            y_coord="y",
-        )
-        result.append(file_name)
-
-        file_name = path.join(path_prefix, "ndvi_analysis.tiff")
-        ndvi_analysis_export = xr.DataArray.to_dataset(
-            ndvi_analysis_composite, dim=None, name="ndvi_analysis"
-        )
-        import_export.export_xarray_to_geotiff(
-            ndvi_analysis_export,
-            file_name,
-            bands=["ndvi_analysis"],
-            crs=projection,
-            x_coord="x",
-            y_coord="y",
-        )
-        result.append(file_name)
-
         ndvi_anomaly = ndvi_analysis_composite - ndvi_baseline_composite
+
+        ## Compute
+
+        ndvi_anomaly = ndvi_anomaly.compute()
+
+        ## Write file
 
         file_name = path.join(path_prefix, "ndvi_anomaly.tiff")
         ndvi_anomaly_export = xr.DataArray.to_dataset(
@@ -221,9 +266,8 @@ class NDVIAnomaly(CubeQueryTask):
             x_coord="x",
             y_coord="y",
         )
-        result.append(file_name)
 
-        return result
+        return [file_name]
 
 
 def create_product_measurement(platform, all_measurements):
@@ -231,15 +275,18 @@ def create_product_measurement(platform, all_measurements):
     if platform in ["SENTINEL_2"]:
         product = "s2_esa_sr_granule"
         measurements = all_measurements + ["coastal_aerosol", "scene_classification"]
+        # Change with S2 WOFS ready
+        water_product = "SENTINEL_2_PRODUCT DEFS"
     else:
         product_match = re.search("LANDSAT_(\d)", platform)
         if product_match:
             product = f"ls{product_match.group(1)}_usgs_sr_scene"
             measurements = all_measurements + ["pixel_qa"]
+            water_product = f"ls{product_match.group(1)}_water_classification"
         else:
             raise Exception(f"invalid platform_name {platform}")
 
-    return product, measurements
+    return product, measurements, water_product
 
 
 def is_dataset_empty(ds: xr.Dataset) -> bool:
