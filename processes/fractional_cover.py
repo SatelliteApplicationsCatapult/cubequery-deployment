58b65839cb9c915f162fb3117929cc354e8278a9
Daniel Tipping <daniel@oldreliable.tech>
2020-06-09 11:16:29 +0100

    Correctly rename land_composite dimensions
    Need to assign the output to `land_composite` otherwise nothing
    happens...

--- processes/fractional_cover.py
+++ processes/fractional_cover.py
@@ -150,7 +150,7 @@ class FractionalCover(CubeQueryTask):
         )
 
         land_composite = geomedian(ds, product, all_measurements)
-        land_composite.rename({"x": "longitude", "y": "latitude"})
+        land_composite = land_composite.rename({"x": "longitude", "y": "latitude"})
 
         # Fractional Cover Classification
 
